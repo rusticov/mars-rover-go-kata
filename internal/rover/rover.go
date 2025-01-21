cd18b33bd5df133136d3bb8972eba71c1ea755a8
Rusticov <knightdestruction@pm.me>
2025-01-21 22:50:17 +0000

    refactor: Introduce rotateRight method on Direction
    Rover.ExecuteCommand iterates through rotate right commands and rotates one at a time.

--- internal/rover/rover.go
+++ internal/rover/rover.go
@@ -1,16 +1,41 @@
 package rover
 
+import "fmt"
+
+type Direction rune
+
+const (
+	North Direction = 'N'
+	East  Direction = 'E'
+	South Direction = 'S'
+	West  Direction = 'W'
+)
+
+func (d Direction) rotateRight() Direction {
+	switch d {
+	case North:
+		return East
+	case East:
+		return South
+	default:
+		return North
+	}
+}
+
 type Rover struct {
 }
 
-func (r Rover) ExecuteCommands(commands string) string {
-	if commands == "R" {
-		return "0:0:E"
+func (r *Rover) ExecuteCommands(commands string) string {
+	direction := North
+
+	for _, command := range commands {
+		switch command {
+		case 'R':
+			direction = direction.rotateRight()
+		}
 	}
-	if commands == "RR" {
-		return "0:0:S"
-	}
-	return "0:0:N"
+
+	return fmt.Sprintf("0:0:%c", direction)
 }
 
 func New() *Rover {
