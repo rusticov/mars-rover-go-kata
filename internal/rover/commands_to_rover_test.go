a15619671c293924ceca463652015baacea45cab
Rusticov <knightdestruction@pm.me>
2025-01-21 19:49:44 +0000

    chore: Rover cna rotate right once at origin

--- internal/rover/commands_to_rover_test.go
+++ internal/rover/commands_to_rover_test.go
@@ -16,4 +16,14 @@ func TestMovingRover(t *testing.T) {
 			t.Errorf("expected 0:0:N but got %s", location)
 		}
 	})
+
+	t.Run("should face east at origin when rotating right", func(t *testing.T) {
+		movingRover := rover.New()
+
+		location := movingRover.ExecuteCommands("R")
+
+		if location != "0:0:E" {
+			t.Errorf("expected 0:0:E but got %s", location)
+		}
+	})
 }
--- internal/rover/rover.go
+++ internal/rover/rover.go
@@ -4,6 +4,9 @@ type Rover struct {
 }
 
 func (r Rover) ExecuteCommands(commands string) string {
+	if commands == "R" {
+		return "0:0:E"
+	}
 	return "0:0:N"
 }
 
