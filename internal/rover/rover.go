b0a9a1395366bb5aeaf0094f3ab202af66dbc43e
Rusticov <knightdestruction@pm.me>
2025-01-22 12:39:48 +0000

    refactor: Parameterize the north coordinate

--- internal/rover/rover.go
+++ internal/rover/rover.go
@@ -6,6 +6,7 @@ type Rover struct {
 }
 
 func (r *Rover) ExecuteCommands(commands string) string {
+	y := 0
 	direction := North
 
 	for _, command := range commands {
@@ -17,7 +18,7 @@ func (r *Rover) ExecuteCommands(commands string) string {
 		}
 	}
 
-	return fmt.Sprintf("0:0:%c", direction)
+	return fmt.Sprintf("0:%d:%c", y, direction)
 }
 
 func New() *Rover {
