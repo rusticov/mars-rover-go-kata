2ba0015066aaa3923b858d734de4423afead4209
Rusticov <knightdestruction@pm.me>
2025-01-25 12:25:17 +0000

    refactor: moveForwards moved from Rover to Grid
    This is a step towards the Rover having its location on the Grid.

--- internal/rover/grid.go
+++ internal/rover/grid.go
@@ -1,8 +1,27 @@
 package rover
 
 type Grid struct {
+	x    int
+	y    int
+	size int
 }
 
-func NewGrid() Grid {
-	return Grid{}
+func (g *Grid) moveForwards(facing direction, x, y int) (int, int) {
+	switch facing {
+	case North:
+		y = (y + 1) % g.size
+	case South:
+		y = (y - 1 + g.size) % g.size
+	case East:
+		x = (x + 1) % g.size
+	case West:
+		x = (x - 1 + g.size) % g.size
+	}
+	return x, y
+}
+
+func NewGrid() *Grid {
+	return &Grid{
+		size: 10,
+	}
 }
--- internal/rover/rover.go
+++ internal/rover/rover.go
@@ -3,6 +3,7 @@ package rover
 import "fmt"
 
 type Rover struct {
+	grid *Grid
 }
 
 func (r *Rover) ExecuteCommands(commands string) string {
@@ -17,29 +18,13 @@ func (r *Rover) ExecuteCommands(commands string) string {
 		case 'R':
 			facing = facing.rotateRight()
 		case 'M':
-			x, y = r.moveForwards(facing, x, y)
+			x, y = r.grid.moveForwards(facing, x, y)
 		}
 	}
 
 	return fmt.Sprintf("%d:%d:%c", x, y, facing)
 }
 
-func (r *Rover) moveForwards(facing direction, x, y int) (int, int) {
-	gridSize := 10
-
-	switch facing {
-	case North:
-		y = (y + 1) % gridSize
-	case South:
-		y = (y - 1 + gridSize) % gridSize
-	case East:
-		x = (x + 1) % gridSize
-	case West:
-		x = (x - 1 + gridSize) % gridSize
-	}
-	return x, y
-}
-
-func New(_ Grid) *Rover {
-	return &Rover{}
+func New(grid *Grid) *Rover {
+	return &Rover{grid: grid}
 }
